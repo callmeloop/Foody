b049128cb61e84eb0ceeb6f7d903d35c4f05f9cf
Ilker Saglik <ilkersaglik.ee@gmail.com>
2023-08-23 15:18:22 +0300

    updates package structure

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foody
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "e2a122533fdb44e58ed64a5358bb91ed"
+    }
+}
